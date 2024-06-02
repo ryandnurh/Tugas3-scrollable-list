d15a8bc8aaa25914ce11ec13e7d2ca04b51c2fba
Febryand Nur Hidayat <ryandnurhidayat@gmail.com>
2024-06-02 15:36:46 +0700

    edit description

--- app/src/main/java/com/example/scrollablelist/halaman2.kt
+++ app/src/main/java/com/example/scrollablelist/halaman2.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
